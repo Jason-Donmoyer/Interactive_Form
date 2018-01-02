621d5c66c5bcdf03be865a80868e1a7efdb7d871
theDuke523 <j.donmoyer523@gmail.com>
2018-01-02 04:22:14 -0500

    changed focus from inline to a js function

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+
+// function to put focus on an element on the form
+function getFocus(id) {
+	document.getElementById(id).focus();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//call the function on the #name id to put focus on the first text input
+getFocus('name');
